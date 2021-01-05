dbde7d9982687cf22fa49d042e71275f4f260ac4
MohammadReza <mr.rahmatzade371@gmail.com>
2021-01-05 10:29:53 +0330

    Add project files.

---
+++ hangfire.Netframework/Program.cs
@@ -0,0 +1,38 @@
+﻿using Hangfire;
+using Hangfire.MemoryStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hangfire.Netframework
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GlobalConfiguration.Configuration.UseMemoryStorage();
+            RecurringJob.AddOrUpdate("test", () => SampleJob.Execute(), Cron.Hourly);
+            RecurringJob.Trigger("test");
+
+            using (new BackgroundJobServer())
+            {
+                Console.ReadLine();
+            }
+
+            
+
+
+
+
+
+
+            //keep the app open to scheduler do his jobs
+            await Task.Delay(1000000000);
+            await Task.Delay(1000000000);
+            await Task.Delay(1000000000);
+            await Task.Delay(1000000000);
+        }
+    }
+}
